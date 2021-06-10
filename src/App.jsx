f2e9fecc537061104c1fa60c22f1cf62d2181f31
Carlos Daniel OB <carlosdosbe@gmail.com>
2021-06-09 19:07:45 -0500

    81. Ruta protegida (parte #1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Admin from './components/Admin'
 import Login from './components/Login'
 import Navbar from './components/Navbar'
 
@@ -24,10 +25,10 @@ function App() {
             <Login/>
           </Route>
           <Route path="/admin">
-            admin
+            <Admin/>
           </Route>
           <Route path="/">
-            index
+            inicio
           </Route>
         </Switch>
       </div>
---
+++ src/components/Admin.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {auth} from '../firebase'
+import {withRouter} from 'react-router-dom'
+
+const Admin = (props) => {
+
+  const [user, setUser] = React.useState(null)
+
+  React.useEffect(() => {
+    if(auth.currentUser){
+      console.log('existe')
+      setUser(auth.currentUser)
+    }else{
+      console.log('no existe')
+      props.history.push('/login')
+    }
+  }, [])
+
+  return (
+    <div>
+      <h2>Protegida</h2>
+    </div>
+  )
+}
+
+export default withRouter(Admin)
